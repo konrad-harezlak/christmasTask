b1253b96e025e0d06a03e42186c3b6f9a4a7e702
konrad-harezlak <konrad.harezlakk@gmail.com>
2024-01-08 17:36:17 +0100

    add welcome message

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pool = require('./pool');
 const router = express.Router();
 
 const packageController = require('./packageController')
@@ -12,7 +13,26 @@ router.put('/setMaternityLeave/:idElf', elfController.setMaternityLeave)
 router.post('/addPackage/:name',packageController.addPackage)
 router.put('/signPackage/:idPackage/:idElf',packageController.signPackage)
 
-router.get('/', (req,res)=>{
-    res.send("Witaj na stronie z elfami")
+router.get('/', async (req,res)=>{
+   
+   try {
+        const packages = await pool.query(`select * from packages`);
+        const elves = await pool.query(`select * from elves`);
+        const responseData = {
+            message: "Witaj na stronie z elfami!",
+            packages: packages.rows,
+            elves: elves.rows
+        };
+    
+        const formattedResponse = JSON.stringify(responseData, null, 2);
+    
+        res.setHeader('Content-Type', 'application/json');
+        res.send(formattedResponse);
+        
+    } catch (error) {
+        console.error("Error adding elf:", error);
+        throw error;
+    }
+    
 })
 module.exports= router;
